36388cc5f23d77dcbf4f5576f29d421e527c7bf8
clay-most <46730958+clay-most@users.noreply.github.com>
2019-04-17 17:20:06 -0600

    objects chalenge complete

--- assignments/objects.js
+++ assignments/objects.js
@@ -29,11 +29,12 @@ const kennan2 = {
   email: "kdiben1@tinypic.com",
   name: "Kennan",
   gender: "m",
+  speak: function() { return `My name is ${this.name}!`}
 };
-const kevin3 = {
+const keven3 = {
   id: 3,
   email: "kmummery2@wikimedia.org",
-  name: "Kevin",
+  name: "Keven",
   gender: "m",
 };
 const gannie4 = {
@@ -47,27 +48,29 @@ const antonietta5 = {
   email: "adaine5@samsung.com",
   name: "Antonietta",
   gender: "f",
-}
+  multiplyNums: function(num1,num2) {return (num1*num2)}
+};
 
 // ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(mitzi1.name);
 // Kennan's ID
-
+console.log(kennan2.id);
 // Keven's email
-
+console.log(keven3.email);
 // Gannie's name
-
-// Antonietta's Gender
+console.log(gannie4.name);
+// Antonietta's gender
+console.log(antonietta5.gender);
 
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta5.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
